5953c80d72301a54774c680b9c6f3277e9e980b2
Devin Smith <devinsmith@deephaven.io>
2022-04-22 13:19:08 -0700

    Use new deephaven

--- server/src/main/app.d/crypto-logic.py
+++ server/src/main/app.d/crypto-logic.py
@@ -1,5 +1,8 @@
 import jpy
-from deephaven import ConsumeKafka as kt
+
+from deephaven import kafka_consumer as kt
+from deephaven.table import Table
+from deephaven.stream.kafka.consumer import TableType, KeyValueSpec
 
 def ops():
     return jpy.get_type('io.deephaven.crypto.Ops')
@@ -11,37 +14,42 @@ def table_creator():
     return jpy.get_type('io.deephaven.engine.table.impl.TableCreatorImpl').INSTANCE
 
 def get_now_table():
-    return table_creator().emptyTable(1).select("Timestamp=DateTime.now()")
+    return Table(j_table=table_creator().emptyTable(1)).select("Timestamp=DateTime.now()")
 
 def get_trades_stream():
-    return kt.consumeToTable(
+    return kt.consume(
         { 'bootstrap.servers' : configuration().bootstrapServers(),
           'schema.registry.url' : configuration().schemaRegistryUrl() },
         configuration().tradesTopic(),
-        key = kt.IGNORE,
-        value = kt.avro(configuration().tradeSchema()),
+        # https://github.com/deephaven/deephaven-core/issues/2305
+        # value_spec = kt.avro_spec(configuration().tradeSchema()),
+        value_spec = kt.avro_spec(configuration().tradesTopicSchemaName()),
         offsets=kt.ALL_PARTITIONS_SEEK_TO_BEGINNING,
-        table_type='stream')\
-        .dropColumns("KafkaPartition", "KafkaOffset", "KafkaTimestamp")
+        table_type=TableType.Stream)
 
 def get_quotes_stream():
-    return kt.consumeToTable(
+    return kt.consume(
         { 'bootstrap.servers' : configuration().bootstrapServers(),
           'schema.registry.url' : configuration().schemaRegistryUrl() },
         configuration().quotesTopic(),
-        key = kt.IGNORE,
-        value = kt.avro(configuration().quoteSchema()),
-        table_type='stream')\
-        .dropColumns("KafkaPartition", "KafkaOffset", "KafkaTimestamp")
+        # https://github.com/deephaven/deephaven-core/issues/2305
+        # value_spec = kt.avro_spec(configuration().quoteSchema()),
+        value_spec = kt.avro_spec(configuration().quotesTopicSchemaName()),
+        table_type=TableType.Stream)
+
+def cast(j_table):
+    # jpy.cast is necessary due to loss of type information due to generics
+    # https://github.com/deephaven/deephaven-core/issues/2329
+    return Table(j_table=jpy.cast(j_table, Table.j_object_type))
 
 def get_trades_summary(trades, usd_prices):
-    return ops().getTradesSummary(trades, usd_prices)
+    return cast(ops().getTradesSummary(trades.j_table, usd_prices.j_table))
 
 def get_quotes_latest(quotes):
-    return ops().getQuotesLatest(quotes)
+    return cast(ops().getQuotesLatest(quotes.j_table))
 
 def get_trades_latest(trades):
-    return ops().getTradesLatest(trades)
+    return cast(ops().getTradesLatest(trades.j_table))
 
 def get_usd_prices(quotes_latest):
-    return ops().getUsdPrices(table_creator(), quotes_latest)
+    return cast(ops().getUsdPrices(table_creator(), quotes_latest.j_table))
--- server/src/main/app.d/crypto-run-implicit.py
+++ server/src/main/app.d/crypto-run-implicit.py
@@ -2,12 +2,12 @@ import jpy
 from typing import Callable
 
 def initialize_implicitly(func: Callable[[], None]):
-    app = jpy.get_type("io.deephaven.appmode.ApplicationContext").get()
+    # app = jpy.get_type("io.deephaven.appmode.ApplicationContext").get()
     new_globals = dict(globals())
     exec(func.__code__, new_globals)
     for key, value in new_globals.items():
         if not key in globals().keys() or globals()[key] != value:
-            app.setField(key, value)
+            # app.setField(key, value)
             globals()[key] = value
 
 initialize_implicitly(crypto_start)
